aca9f6f485570eee89bfc206001ef3810f5b92b7
Mathieu Muller <mathieu@unity3d.com>
2015-12-03 19:20:03 +0100

    Add Simulation

---
+++ 3DGeoSciences/Assets/Scripts/RaycastSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastSpawner : MonoBehaviour {
+
+	public GameObject prefab;
+
+	private float lastTimeMouseButtonDown;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			lastTimeMouseButtonDown = Time.time;
+		}
+
+		if (Input.GetMouseButtonUp(0) && Time.time - lastTimeMouseButtonDown < 0.1f)
+		{
+			SpawnOnHit();
+		}
+	}
+
+	void SpawnOnHit()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit) == true)
+		{
+			GameObject go = (GameObject) Instantiate(prefab, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+			go.transform.SetParent(transform);
+		}
+	}
+}
