9b632474ff47210a490cd3ad10f55f26ab6db2a1
Mathieu Muller <mathieu@unity3d.com>
2015-12-03 14:28:11 +0100

    Add Camera Orbit

--- 3DGeoSciences/Assets/OrbitCamera.cs
+++ 3DGeoSciences/Assets/OrbitCamera.cs
@@ -3,10 +3,34 @@ using System.Collections;
 
 public class OrbitCamera : MonoBehaviour {
 
+	public float _distance = 3f;
+	public float _sensitivity = 10f;
 
+	float _currentRotationRight = 0f;
+	float _currentRotationUp = 0f;
+
+	void Start()
+	{
+		Rotate (_currentRotationRight, _currentRotationUp);
+	}
 
 	void Update ()
 	{
-			
+		if (Input.GetMouseButton(0))
+		{ 	
+			_currentRotationRight += Input.GetAxis("Mouse Y") * _sensitivity;
+			_currentRotationUp += Input.GetAxis("Mouse X") * _sensitivity;
+			//Rotate(_currentRotationRight, _currentRotationUp);
+		}
+
+		Rotate(_currentRotationRight, _currentRotationUp);
+	}
+
+	void Rotate(float degreesAroundRight, float degreesAroundUp)
+	{
+		Quaternion rotation = Quaternion.Euler(-degreesAroundRight, degreesAroundUp, 0f);
+		transform.rotation = rotation;
+		Vector3 percheASelfie = new Vector3(0f, 0f, -_distance); // TODO: Move new outside of update
+		transform.position = rotation * percheASelfie;
 	}
 }
